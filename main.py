76fd1f674b20ec8507d26df6a30237cc64e16120
Anish <anishpandey021@gmail.com>
2026-01-04 19:17:21 -0600

    refactor: Reformat `consolidated_purchases.csv` by standardizing dates to YYYY-MM-DD, adding year 2025, and sorting entries.

--- main.py
+++ main.py
@@ -2,5 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df=pd.read_csv("data/consolidated_purchases.csv")
-df['Date']=pd.to_datetime(df['Date'])
-print(df.head())
+
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
+
+weekly_spending = df.resample('W')['Cost'].sum()
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(weekly_spending.index, weekly_spending.values, marker='o', linestyle='-')
+ax.set_xlabel('Date')
+ax.set_ylabel('Weekly Cost')
+ax.set_title('Weekly Spending Over Time')
+plt.grid(True)
+plt.show()
+
+print(weekly_spending.head())
